40c2b7d42aaa2da32cc4d20d931e347886f05568
Daniel Narvaez <dwnarvaez@gmail.com>
2013-05-21 22:00:20 +0200

    Use sugar-html-test as karma browser

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -58,7 +58,7 @@ autoWatch = true;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = [];
+browsers = ["sugar-html-test"];
 
 
 // If browser does not capture in given timeout [ms], kill it
