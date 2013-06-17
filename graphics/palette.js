a0a2b84699c0f7ce36c1a38cb4ed34b4cce0a0b4
Manuel Quiñones <manuq@laptop.org>
2013-06-17 11:20:25 -0300

    More on palettes
    Only one palette visible per activity.  A palette popup provokes the
    popdown of the current visible one, if any.
    
    If the invoker is a toolbar button, add a background box on top of the
    palette, over the button.
    
    If the invoker is not a toolbar button, the palette will popup in the
    clicked position.

--- graphics/palette.js
+++ graphics/palette.js
@@ -1,13 +1,19 @@
 define(function () {
+    var palettesGroup = [];
 
     function getOffset(elem) {
-        var x = 0;
-        var y = 0;
+        // Ugly hack to consider the palette margin.
+        var style = elem.currentStyle || window.getComputedStyle(elem, '');
+
+        // Remove 'px' from the strings.
+        var x = -2 * style.marginLeft.slice(0, -2);
+        var y = -1 * style.marginTop.slice(0, -2);
+
         var width = elem.offsetWidth;
         var height = elem.offsetHeight;
         while (elem) {
-            x += elem.offsetLeft - elem.scrollLeft;
-            y += elem.offsetTop - elem.scrollTop;
+            x += elem.offsetLeft - elem.scrollLeft + elem.clientLeft;
+            y += elem.offsetTop - elem.scrollTop + elem.clientTop;
             elem = elem.offsetParent;
         }
         return {
@@ -22,50 +28,103 @@ define(function () {
 
     palette.Palette = function (invoker) {
         this.invoker = invoker;
-        var container;
+        var paletteElem;
+        var containerElem;
         var that = this;
+        palettesGroup.push(this);
 
-        function updatePosition() {
-            var invokerOffset = getOffset(that.invoker);
-            container.style.top = invokerOffset.top +
-                invokerOffset.height + "px";
-            container.style.left = invokerOffset.left + "px";
+        invoker.addEventListener('click', function (event) {
+            if (!that.invoker.classList.contains("toolbutton")) {
+                updatePosition(event.x, event.y);
+            }
+            that.toggle();
+        });
+
+        function updatePosition(clickX, clickY) {
+            var paletteX;
+            var paletteY;
+
+            if (typeof (clickX) !== 'undefined' &&
+                typeof (clickY) !== 'undefined') {
+                paletteX = clickX;
+                paletteY = clickY;
+            } else {
+                var invokerOffset = getOffset(that.invoker);
+                paletteX = invokerOffset.left;
+                paletteY = invokerOffset.top;
+            }
+
+            paletteElem.style.left = paletteX + "px";
+            paletteElem.style.top = paletteY + "px";
         }
 
-        // create a new container for the content of the palette, removing
-        // the previous content if it had any
+        // create a new palette element with a container, removing the
+        // previous content if it had any
 
-        function createContainer() {
-            if (container !== undefined) {
-                document.body.removeChild(container);
+        function createPalette() {
+            if (paletteElem !== undefined) {
+                return;
             }
-            container = document.createElement('div');
-            container.className = "palette";
-            container.style.visibility = "hidden";
-            document.body.appendChild(container);
+            paletteElem = document.createElement('div');
+            paletteElem.className = "palette";
+            paletteElem.style.visibility = "hidden";
+            document.body.appendChild(paletteElem);
+
+            if (that.invoker.classList.contains("toolbutton")) {
+                invokerElem = document.createElement('div');
+                invokerElem.className = "palette-invoker";
+                var style = that.invoker.currentStyle ||
+                    window.getComputedStyle(that.invoker, '');
+                invokerElem.style.backgroundImage = style.backgroundImage;
+
+                invokerElem.addEventListener('click', function (e) {
+                    that.toggle();
+                });
+
+                paletteElem.appendChild(invokerElem);
+
+            }
+
+            containerElem = document.createElement('div');
+            containerElem.className = "palette-container";
+            paletteElem.appendChild(containerElem);
+
             updatePosition();
         }
 
-        this.getContainer = function () {
-            if (container === undefined) {
-                createContainer();
+        this.getPalette = function () {
+            if (paletteElem === undefined) {
+                createPalette();
             }
-            return container;
+            return paletteElem;
+        };
+
+        this.getContainer = function () {
+            if (paletteElem === undefined) {
+                createPalette();
+            }
+            return containerElem;
         };
 
         this.isDown = function () {
-            return container === undefined ||
-                container.style.visibility == "hidden";
+            return paletteElem === undefined ||
+                paletteElem.style.visibility == "hidden";
         };
 
     };
 
     palette.Palette.prototype.popUp = function () {
-        this.getContainer().style.visibility = "visible";
+        for (var i = 0; i < palettesGroup.length; i++) {
+            otherPalette = palettesGroup[i];
+            if (otherPalette != this) {
+                otherPalette.popDown();
+            }
+        }
+        this.getPalette().style.visibility = "visible";
     };
 
     palette.Palette.prototype.popDown = function () {
-        this.getContainer().style.visibility = "hidden";
+        this.getPalette().style.visibility = "hidden";
     };
 
     palette.Palette.prototype.toggle = function () {
