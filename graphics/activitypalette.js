0a7a2a1ea80275a9064e81cc746947e1ca5afa31
surajgillespie <suraj_gillespie@yahoo.co.in>
2013-07-08 04:40:24 +0530

    Link activity palette with datastore

--- graphics/activitypalette.js
+++ graphics/activitypalette.js
@@ -31,6 +31,64 @@ define(["sugar-web/graphics/palette",
         descriptionBox.className = "expand";
 
         this.setContent([activityTitle, descriptionLabel, descriptionBox]);
+
+        var aTitle = document.getElementById("title");
+        var activityDescription = document.getElementById("description");
+        var datastoreObject = new datastore.DatastoreObject();
+        var mdata;
+        var Title;
+        
+        bus.listen();
+        
+        datastoreObject.getMetadata(function (error, metadata) {
+            mdata = metadata;
+            setTitleDescription();
+        });
+
+        setTitleDescription();
+
+        function setTitleDescription() {
+            if ((mdata === undefined) || (mdata.title == undefined)) {
+                env.getEnvironment(function (error, environment) {
+                    Title = environment.activityName;
+                    datastoreObject.setMetadata({
+                        "activity": environment.bundleId,
+                        "activity_id": environment.activityId,
+                        "title": Title
+                    });
+                    datastoreObject.save(function () {});
+                    aTitle.value = Title;
+                });
+            } else {
+                if (mdata.title != null) {
+                    aTitle.value = mdata.title;
+                    datastoreObject.setMetadata({
+                        "activity": environment.bundleId,
+                        "activity_id": environment.activityId,
+                        "title": mdata.title
+                    });
+                    datastoreObject.save(function () {});
+                }
+
+                if (mdata.description != null)
+                    activityDescription.value = mdata.description;
+
+            }
+        }
+
+        aTitle.onblur = function () {
+            datastoreObject.setMetadata({
+                "title": aTitle.value
+            });
+            datastoreObject.save(function () {});
+        };
+
+        activityDescription.onblur = function () {
+            datastoreObject.setMetadata({
+                "description": activityDescription.value,
+            });
+            datastoreObject.save(function () {});
+        };
     
     return Activitypalette;
 });
