560f844116b93227507ddf6c8be0d2e7176230d1
Manuel Quiñones <manuq@laptop.org>
2013-07-30 16:42:36 -0300

    Make optional the callback of DatastoreObject.save

--- datastore.js
+++ datastore.js
@@ -115,6 +115,10 @@ define(["sugar-web/bus", "sugar-web/env"], function (bus, env) {
     };
 
     DatastoreObject.prototype.save = function (callback) {
+        if (callback === undefined) {
+            callback = function () {};
+        }
+
         var that = this;
 
         function onCreated(error, objectId) {
