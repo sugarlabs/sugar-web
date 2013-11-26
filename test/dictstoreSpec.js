d3d61c1bfcda1b8d90824887357169cfa6d20275
Rogelio Mita <rogeliomita@gmail.com>
2013-11-25 23:48:25 -0300

    dictstore save method uses isStandalone method

--- test/dictstoreSpec.js
+++ test/dictstoreSpec.js
@@ -7,27 +7,47 @@ define(["sugar-web/dictstore", "sugar-web/env"], function (dictstore, env) {
       describe("init method", function () {
 
         it("should execute callback", function () {
-
           spyOn(env, 'isStandalone').andReturn(true);
           var callback = jasmine.createSpy();
 
           dictstore.init(callback);
           expect(callback).toHaveBeenCalled();
-
         });
 
         it("should maintain localStorage", function () {
-
           spyOn(env, 'isStandalone').andReturn(true);
           localStorage.test_key = "test";
 
           dictstore.init(function () {});
           expect(localStorage.test_key).toBe("test");
-
         });
+      });
 
+      describe("save method", function () {
+
+        it("should just execute the callback", function () {
+          spyOn(env, 'isStandalone').andReturn(true);
+          var callback_executed;
+
+          localStorage.test_key = "test";
+
+          runs(function () {
+            callback_executed = false;
+
+            dictstore.save(function () {
+              callback_executed = true;
+            });
+          });
+
+          waitsFor(function () {
+            return callback_executed === true;
+          }, "The callback should executed");
+
+          runs(function () {
+            expect(localStorage.test_key).toBe("test");
+          });
+        });
       });
 
     });
-
 });
