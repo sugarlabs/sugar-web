9702d28312b76046a72ea50ae1934cfd2d95e7c7
Lionel Laské <llaske@c2s.fr>
2014-02-26 08:40:23 +0100

    Merge with Sugarizer handling

--- env.js
+++ env.js
@@ -8,7 +8,19 @@ define(function () {
         // FIXME: we assume this code runs on the same thread as the
         // javascript executed from sugar-toolkit-gtk3 (python)
 
-        if (env.isStandalone()) {
+        if (env.isSugarizer()) {
+            var getUrlParameter = function(name) {
+                var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+                return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+            };
+            window.top.sugar = {};
+            window.top.sugar.environment = {};
+            window.top.sugar.environment.activityId = getUrlParameter("aid");
+            window.top.sugar.environment.activityName = getUrlParameter("n");
+            window.top.sugar.environment.bundleId = getUrlParameter("a");
+            window.top.sugar.environment.objectId = getUrlParameter("o");
+			callback(null, window.top.sugar.environment);
+        } else if (env.isStandalone()) {
             setTimeout(function () {
                 callback(null, {});
             }, 0);
@@ -36,6 +48,10 @@ define(function () {
         });
     };
 
+    env.getURLScheme = function () {
+        return window.location.protocol;
+    };
+
     env.getHost = function() {
         return window.location.hostname;
     };
@@ -45,6 +61,17 @@ define(function () {
         var currentHost = env.getHost();
 
         return currentHost !== webActivityHost;
+    };
+    
+    env.isSugarizer = function() {
+        if (typeof(Storage)!=="undefined" && typeof(window.localStorage)!=="undefined") {
+            try {
+                return (window.localStorage.getItem('sugar_settings') !== null);
+            } catch(err) {
+                return false;
+            }
+        }
+        return false;
     };
 
     return env;
